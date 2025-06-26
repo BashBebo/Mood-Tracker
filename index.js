3a72b0dbffb76703adcbc4a5b7a0d3dc08a658d4
Bashir <bashir6412@gmail.com>
2025-06-26 21:40:31 +0400

    Javascript commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('MiniMood Tracker on track!')
+const moodbutton = document.querySelectorAll('.mood-btn');
+let selectedMood = null
+
+moodbutton.forEach(button => {
+    button.addEventListener('click', () => {
+        moodbutton.forEach(btn => btn.classList.remove('selected'));
+        button.classList.add('selected');
+    selectedMood = button.dataset.mood;
+    });
+});
+
+const LogMood = document.getElementById('LogMood')
+LogMood.addEventListener('click', () => {
+    if (selectedMood) {
+        alert(`You have logged your mood as: ${selectedMood}`);
+    } else {
+        alert('Please select a mood before logging.');
+    }
+})
